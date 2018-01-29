b1d395e4bbda0b44ef01f41e311101ada68751ec
guimms <migeot-guillaume@hotmail.com>
2018-01-29 12:54:01 +0100

    vue tout les metier (fait)

--- src/Controller/JobsController.php
+++ src/Controller/JobsController.php
@@ -8,6 +8,7 @@ use App\Form\ModifJob;
 use App\Repository\JobsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -20,12 +21,14 @@ class JobsController extends Controller
      * @Route("/vueJobs", name="vueJobs")
      */
     public function detailsAction(){
-        $repo = $this->getDoctrine()->getRepository("Jobs");
-        $jeux = $repo->findAll();
+        $repo = $this->getDoctrine()->getRepository(Jobs::class)
+        ->findAll();
+
+//        return new Response('Check out this great product: '.$repo->getnameJobs());
 
         return $this->render(
-            'metier.html.twig',
-            array('name' => $jeux)
+            'metier.html.twig', ['Jobs' => $repo]
+
         );
     }
 
