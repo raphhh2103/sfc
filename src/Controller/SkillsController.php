77de7500131a897fce2449556f2d4ea82f90bdd9
raphaelk <raphaelk@live.be>
2018-01-31 15:01:14 +0100

    skills create !! en 2/2 respect ma poule mdrr

--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Skills;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SkillsController extends Controller
@@ -11,9 +16,28 @@ class SkillsController extends Controller
     /**
      * @Route("/skills", name="skills")
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $skills = new Skills();
+        $form = $this->createFormBuilder($skills)
+            ->add('name',TextType::class)
+
+            ->add('save',SubmitType::class ,array('label'=> 'create skills'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($skills);
+            $em->flush();
+
+        }
+
+
         // replace this line with your own code!
-        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+        return $this->render('Formateur/skills.html.twig',array('form'=>$form->createView()));
     }
 }
