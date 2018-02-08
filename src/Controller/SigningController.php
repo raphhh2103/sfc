31d54fa427b9c9d55abefe02fa244aee64dee246
raphaelk <raphaelk@live.be>
2018-02-08 13:19:54 +0100

    Auto stash before merge of "web" and "origin/web"

--- src/Controller/SigningController.php
+++ src/Controller/SigningController.php
@@ -121,82 +121,51 @@ class SigningController extends Controller
         foreach ($jobSfc as $key => $values) {
 //            dump($values);
             if ($values->getUser() === $user) {
-                if ($key === 0)
-                    $currentJobSfc = $values;
+                if ($key === 0) {
+                    $currentJobSfc[0] = $values;
 //                dump($currentJobSfc);
-                dump('0');
+                    dump('0');
+                }
                 if ($key === 1) {
-                    $currentJobSfc1 = $values;
+                    $currentJobSfc[1] = $values;
 //                    dump($currentJobSfc1);
                     dump('1');
                 }
                 if ($key === 2) {
-                    $currentJobSfc2 = $values;
+                    $currentJobSfc[2] = $values;
+
                     dump('2');
                 }
                 if ($key === 3) {
-                    $currentJobSfc3 = $values;
-                    dump('3');
+                    $currentJobSfc[3] = $values;
+                    dump($currentJobSfc);
+
                 }
                 if ($key === 4) {
-                    $currentJobSfc4 = $values;
+                    $currentJobSfc[4] = $values;
                     dump('4');
+
                 }
                 if ($key === 5) {
-                    $currentJobSfc5 = $values;
+                    $currentJobSfc[5] = $values;
                     dump('5');
-                }
-                if ($key === 6) {
-                    $currentJobSfc6 = $values;
-                    dump('6');
+
                 }
 
 
+                return $this->render('result/graphic.html.twig', array(
+                    'value' => $value,
+                    'sfc'=>$currentJobSfc,
+                    'jobs'=>$currentJobSfc[0]->getJobs(),
+                ));
             }
         }
 
 
         return $this->render('result/graphic.html.twig', array(
             'value' => $value,
-            'sfc1' => $currentJobSfc->getSfc()->getNameSfc(),
-            'sfc2' => $currentJobSfc1->getSfc()->getNameSfc(),
-            'sfc3' => $currentJobSfc2->getSfc()->getNameSfc(),
-//            'sfc4'=>$currentJobSfc3->getSfc()->getNameSfc(),
-//            'sfc5'=>$currentJobSfc4->getSfc()->getNameSfc(),
-//            'sfc6'=>$currentJobSfc5->getSfc()->getNameSfc(),
-            'sfc1ig1' => $currentJobSfc->getIndicatorGeneric1(),
-            'sfc1ig2' => $currentJobSfc->getIndicatorGeneric2(),
-            'sfc1ig3' => $currentJobSfc->getIndicatorGeneric3(),
-            'sfc1ig4' => $currentJobSfc->getIndicatorGeneric4(),
-            'sfc2ig1' => $currentJobSfc1->getIndicatorGeneric1(),
-            'sfc2ig2' => $currentJobSfc1->getIndicatorGeneric2(),
-            'sfc2ig3' => $currentJobSfc1->getIndicatorGeneric3(),
-            'sfc2ig4' => $currentJobSfc1->getIndicatorGeneric4(),
-            'sfc3ig1' => $currentJobSfc2->getIndicatorGeneric1(),
-            'sfc3ig2' => $currentJobSfc2->getIndicatorGeneric2(),
-            'sfc3ig3' => $currentJobSfc2->getIndicatorGeneric3(),
-            'sfc3ig4' => $currentJobSfc2->getIndicatorGeneric4(),
-//            'sfc4ig1'=>$currentJobSfc3->getIndicatorGeneric1(),
-//            'sfc4ig2'=>$currentJobSfc3->getIndicatorGeneric2(),
-//            'sfc4ig3'=>$currentJobSfc3->getIndicatorGeneric3(),
-//            'sfc4ig4'=>$currentJobSfc3->getIndicatorGeneric4(),
-//            'sfc5ig1'=>$currentJobSfc4->getIndicatorGeneric1(),
-//            'sfc5ig2'=>$currentJobSfc4->getIndicatorGeneric2(),
-//            'sfc5ig3'=>$currentJobSfc4->getIndicatorGeneric3(),
-//            'sfc5ig4'=>$currentJobSfc4->getIndicatorGeneric4(),
-//            'sfc6ig1'=>$currentJobSfc5->getIndicatorGeneric1(),
-//            'sfc6ig2'=>$currentJobSfc2->getIndicatorGeneric2(),
-//            'sfc6ig3'=>$currentJobSfc3->getIndicatorGeneric3(),
-//            'sfc6ig4'=>$currentJobSfc4->getIndicatorGeneric4(),
-            'requiredLevel' => $currentJobSfc->getRequiredLevel(),
-            'requiredlevel1' => $currentJobSfc1->getRequiredLevel(),
-//            'requiredlevel2'=>$currentJobSfc2->getRequiredLevel(),
-//            'requiredlevel3'=>$currentJobSfc3->getRequiredLevel(),
-//            'requiredlevel4'=>$currentJobSfc4->getRequiredLevel(),
-            'jobs' => $currentJobSfc->getJobs()->getNameJobs(),
-
-
-
+            'sfc'=>$currentJobSfc,
+            'jobs'=>$currentJobSfc[0]->getJobs(),
         ));
     }
 
